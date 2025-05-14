ad1b13297f8c2dcca55c127aa8d4d86a50debd07
ayshamusthafa <ayshakmd2@gmail.com>
2025-05-14 18:46:16 +0530

    Add files via upload

---
+++ JS/srollprogess.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+    window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+</script>
